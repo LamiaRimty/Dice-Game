7e8818fa29ec54b74908f4da0c714c3b4f0fe470
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-09-25 15:46:28 +0200

    Condition applied

---
+++ index.js
@@ -0,0 +1,25 @@
+
+var randomNumber1=Math.floor(Math.random()*6)+1;//1-6
+var randomDiceImage="dice"+randomNumber1+".png";  // dice1.png-dice6.png
+var randomDiceImageSource= "images/"+randomDiceImage; //images/dice1.png-images/dice6.png
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomDiceImageSource);
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var randomDiceImage2="dice"+randomNumber2+".png";
+var randomDiceImageSource2="images/"+randomDiceImage2; //images/dice1.png-images.dice6.png
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomDiceImageSource2);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🏆 Player 1 Wins!";
+}
+
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Wins!🏆 ";
+}
+
+else{
+    document.querySelector("h1").innerHTML="🤞🏻 Draw!";
+}
